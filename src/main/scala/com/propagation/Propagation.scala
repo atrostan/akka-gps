af30090e117fb8e0b9404f5bf557122c77071aac
DSep <dyanatkar@me.com>
2021-10-16 21:33:54 -0700

    Cleaned up, clarified. Finds path with certain number of elems

--- src/main/scala/com/propagation/Propagation.scala
+++ src/main/scala/com/propagation/Propagation.scala
@@ -14,26 +14,6 @@ import com.example.VertexMain.StartProcessing // ASK Why does this work
 import scala.collection.mutable.ArrayBuffer
 import scala.collection.mutable.Map
 
-/**
-  Behviour for IdRequest message
-    if acceptingRequests:
-      disable acceptingRequests
-      if hasNeighbours:
-        message neighbors requesting info
-      else:
-        send response with own id to requestor
-    else:
-      send response with no id to requestor
-
-
-  Behaviour for IdResponse message
-    add given id/data to list of collected data
-    if len list == number of neighbours:         // Could also check that the data contains all the neighbours
-      send IdResponse to parent, containing list
-      Behaviors.stopped
-
-  */
-
 object Vertex {
   sealed trait VertexMessage
   final case class Request(from: Int, replyTo: ActorRef[VertexMessage]) extends VertexMessage
@@ -42,50 +22,44 @@ object Vertex {
   
   // Variables here are shared across instances
   // var data = ArrayBuffer[Int]()
+  
+  // HACK graph data
+  private var outNeighborsMap: Map[Int,Set[Int]] = null
+  private var vertexRefs: Vector[ActorRef[VertexMessage]] = null
 
-  // TODO Alt design pattern
+  // TODO Functional style design pattern usually has this instead of directly writing it in apply
+  // see: https://doc.akka.io/docs/akka/current/typed/style-guide.html
   // def apply(max: Int): Behavior[VertexMessage] = {
   //   doStuff(0, max)
   // }
 
   def apply(id: Int): Behavior[VertexMessage] = {
-    // HACK graph data
-    var outNeighborsMap: Map[Int,Set[Int]] = null
-    var vertexRefs: Vector[ActorRef[VertexMessage]] = null
-
-    var allData = ArrayBuffer[Int]()
-    var acceptingRequests = true    // NOT ROBUST
-    var parent = 0 // id of replyTo, since variable is not available when response needs to be run
+    var allData = ArrayBuffer[Vector[Int]]() // currently will hold all paths found
+    var parent = 0 // id of replyTo, since req message is not available in response
 
     Behaviors
       .receive[VertexMessage] { (context, message) =>
         message match {
           case Request(from, replyTo) => 
             context.log.info("{} requested data from {}", from, id.toString())
-            // if (acceptingRequests) {
-            //   acceptingRequests = false
-              parent = from
-              if (outNeighborsMap(id).size > 0) {
-                outNeighborsMap(id).foreach(neigh => {
-                  val neighRef = vertexRefs(neigh)
-                  neighRef ! Request(id, context.self)
-                })
-              } else {
-                replyTo ! Response(id, Vector(id))
-              }
-            // } else { // happens when vertex is requested multiple timees
-            //   replyTo ! Response(id, Vector())
-            // }
+            parent = from
+            if (outNeighborsMap(id).size > 0) {
+              outNeighborsMap(id).foreach(neigh => {
+                val neighRef = vertexRefs(neigh)
+                neighRef ! Request(id, context.self)
+              })
+            } else {
+              replyTo ! Response(id, Vector(id))
+            }
             Behaviors.same
           case Response(from, data) => 
             context.log.info("{} sent response data to {}:   {}", from, id.toString(), data.toString())
-            allData ++= data
+            allData += data
+            if (id == 0) println("vertex0 has", allData, allData, "just got data", data)
+            
             // Special case for source vertex = 0 //TODO extract away to system or parent
-            if (id == 0) {
-              println("vertex0 has", allData.toSet)
-            }
-            if (id == 0 && allData.toSet.size == 5) {
-                println("COMPLETE. Result: {}", data)
+            if (id == 0 && data.toSet.size == vertexRefs.size - 1) { // ignore extra vertex 0
+                println("Found path with all elements! Result:", data)
                 Behaviors.stopped // REVIEW
             } else if (id != 0 && data.size > 0) { // if the message has any data
               // add given id/data to list of collected data
@@ -98,7 +72,7 @@ object Vertex {
               // } else {
               //   Behaviors.same
               // }
-              
+
               // Alternative response strategy: forward everything received
               val parentRef = vertexRefs(parent)
               parentRef ! Response(id, data.toVector :+ id)
@@ -120,7 +94,6 @@ object Vertex {
   }
 }
 
-//#greeter-main
 object VertexMain {
   final case class SayHello(name: String)
   final case class StartProcessing(nodes: Vector[Int], outNeighborsMap: Map[Int,Set[Int]])
@@ -132,36 +105,33 @@ object VertexMain {
       Behaviors.receiveMessage { message =>
         context.log.info("starter message {}", message)
 
-        // Create all the vertices // TODO Split into separate message
+        // TODO Split into separate message (but still needs to happen before running algo)
+        // Create all the vertices
         message.nodes.foreach(n => {
           println(n)
           allVertices = allVertices :+ context.spawn(Vertex(id = n), "vertex" + n.toString()) // save ref to vertex
         })
         context.log.info("list of all vertex actors {}", allVertices)
         // send all the graph data, now that all ActorRefs are available
-        allVertices.foreach(actor => {
-          actor ! Vertex.SendGraphInfo(message.outNeighborsMap, allVertices)
-        })
+        // allVertices.foreach(actor => {
+        //   actor ! Vertex.SendGraphInfo(message.outNeighborsMap, allVertices)
+        // })
 
         // Startup processing
-        // val source: ActorRef[Vertex.VertexMessage] = context.spawn(Vertex(id = 0, outNeighborsMap = message.outNeighborsMap, vertexRefs = allVertices), "vertex0") 
         val source: ActorRef[Vertex.VertexMessage] = allVertices(0)
         val firstVertex: ActorRef[Vertex.VertexMessage] = allVertices(1)
+        firstVertex ! Vertex.SendGraphInfo(message.outNeighborsMap, allVertices)
         firstVertex ! Vertex.Request(0, source)
         Behaviors.same
       }
     }
 }
-//#greeter-main
 
-//#main-class
 object Propagation extends App {
-  //#actor-system
-  val greeterMain: ActorSystem[VertexMain.StartProcessing] = ActorSystem(VertexMain(), "GraphPropagationTest")
-  //#actor-system
+  val vertexMain: ActorSystem[VertexMain.StartProcessing] = ActorSystem(VertexMain(), "GraphPropagationTest")
 
-  val g = Graph(0~>1, 1~>2, 2~>3, 1~>3, 1~>5, 3~>5, 3~>4, 4~>4, 4~>5) // , 4~>4
-  //NOTE this values on the right are wrong with vertex 0 
+  val g = Graph(0~>1, 1~>2, 2~>3, 1~>3, 1~>5, 3~>5, 3~>4, 4~>4, 4~>5) // NOTE there is an edge from 4->4
+  //NOTE values on the right are wrong with vertex 0 
   println(g.order)                                // Int = 5
   println(g.graphSize)                            // Int = 8
   println(g.size)                                 // Int = 13
@@ -173,7 +143,7 @@ object Propagation extends App {
   println(g.degreeNodesMap(degreeFilter = _ > 3)) // Map(4 -> Set(3,4))
   println(g.nodes)                                // TODO
   
-  // Convert list to Ints and create outNeighbour map to avoid needing g.NodeT
+  // HACK Convert list to Ints and create outNeighbour map to avoid needing g.NodeT
   val nodesVec = ArrayBuffer[Int]()
   var outNeighborsMap = Map[Int, Set[Int]]()
   g.nodes.foreach(n => {
@@ -183,17 +153,27 @@ object Propagation extends App {
     var outNeighbors = Set[Int]()
     n.outNeighbors.foreach[Unit](neigh => outNeighbors += neigh.value)
     outNeighborsMap += (n.value -> outNeighbors)
-  })                              // TODO
+  })
 
-  // Convert to Map[Int, Set[Int]] to avoid needing g.NodeT
-  // g.degreeNodesMap.foreach(kv => {
-  //   var neighborSet = Set[Int]()
-  //   kv._2.foreach[Unit](n => neighborSet += n.value)
-  //   outNeighborsMap += (kv._1 -> neighborSet)
-  // })
-
-  greeterMain ! StartProcessing(nodesVec.toVector, outNeighborsMap)
+  vertexMain ! StartProcessing(nodesVec.toVector, outNeighborsMap)
 }
-//#main-class
-//#full-example
 
+/**
+ Scrap algorithm work
+  Behviour for IdRequest message
+    if acceptingRequests:
+      disable acceptingRequests
+      if hasNeighbours:
+        message neighbors requesting info
+      else:
+        send response with own id to requestor
+    else:
+      send response with no id to requestor
+
+
+  Behaviour for IdResponse message
+    add given id/data to list of collected data
+    if len list == number of neighbours:         // Could also check that the data contains all the neighbours
+      send IdResponse to parent, containing list
+      Behaviors.stopped
+  */
