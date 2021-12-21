359b55cf8cbe350f1ed61fdf80d7876970aa79ad
Joseph Poremba <joe@Josephs-MacBook-Pro-14.local>
2021-12-21 10:25:54 -0500

    Cleanup of algorithm code before project submission

--- src/main/scala/com/algorithm/WCC.scala
+++ src/main/scala/com/algorithm/WCC.scala
@@ -1,41 +1,27 @@
 package com.algorithm
 
-import scalax.collection.edge.Implicits._
-import scalax.collection.Graph // or scalax.collection.mutable.Graph
-import scalax.collection.GraphPredef._, scalax.collection.GraphEdge._
-import scalax.collection.edge.WDiEdge
-
 object WCC extends VertexProgram[Int, Int, Int, Int] {
 
   override val mode: VertexProgram.Mode = VertexProgram.Bidirectional
 
-  override def gather(edgeVal: Int, message: Int): Int = {
-    message
-  }
+  override def gather(edgeVal: Int, message: Int): Int = message
 
-  override def sum(a: Int, b: Int): Int = {
-    Math.min(a, b)
-  }
+  override def sum(a: Int, b: Int): Int = Math.min(a, b)
 
   override def apply(superStepNumber: Int, thisVertex: VertexInfo, oldVal: Int, total: Option[Int]): Int = {
-    if(superStepNumber == 0) {
-      thisVertex.id
-    } else total match {
+    if(superStepNumber == 0) thisVertex.id
+    else total match {
       case Some(componentId) => Math.min(oldVal, componentId)
-      case None => oldVal
+      case None              => oldVal
     }
   }
 
   override def scatter(superStepNumber: Int, thisVertex: VertexInfo, oldVal: Int, newVal: Int): Option[Int] = {
-    if(newVal < oldVal) {
-      Some(newVal)
-    } else {
-      assert(newVal == oldVal, s"Unexpected newVal=${newVal}, oldVal=${oldVal}")
-      None
-    }
+    if(newVal < oldVal) Some(newVal)
+    else None
   }
 
-  override def voteToHalt(superStepNumber: Int, oldVal: Int, newVal: Int): Boolean = true
+  override def deactivateSelf(superStepNumber: Int, oldVal: Int, newVal: Int): Boolean = true
 
   override val defaultVertexValue: Int = Integer.MAX_VALUE
 
